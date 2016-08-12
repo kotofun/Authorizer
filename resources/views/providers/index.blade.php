f25bf9613b8fc1beb9ddf2f7e94946daee1a5529
Дмитрий Басавин <basavind@gmail.com>
2016-08-12 09:51:10 +0300

    refactor index view

--- resources/views/providers/index.blade.php
+++ resources/views/providers/index.blade.php
@@ -4,36 +4,36 @@
     <div class="content">
         <div class="content_auth">
             <h3>Авторизация</h3>
-            <form method="post" id="login-form">
-                <input type="text" name="username" required="" placeholder="Логин"/>
-                <input type="password" name="userpass" required="" placeholder="Пароль"/>
+            <form method="POST" id="login-form">
+                <input type="email" name="email" required="required" placeholder="Email"/>
+                <input type="password" name="password" required="required" placeholder="Пароль"/>
                 <button type="submit" form="login-form" class="white_button">Войти</button>
                 <a href="#"><p class="cursive">Забыли пароль?</p></a>
-
-                <div class="content_auth_divider"></div>
-                <p>Войти, используя аккаунты в социальных сетях:</p>
-                <ul class="content_auth_social">
-                    <a href="{{ route('socialize.request', ['provider' => 'facebook']) }}">
-                        <li class="content_auth_social-item fb-item"></li>
-                    </a>
-                    <a href="{{ route('socialize.request', ['provider' => 'vkontakte']) }}">
-                        <li class="content_auth_social-item vk-item"></li>
-                    </a>
-                    <a href="{{ route('socialize.request', ['provider' => 'twitter']) }}">
-                        <li class="content_auth_social-item tw-item"></li>
-                    </a>
-                    <a href="{{ route('socialize.request', ['provider' => 'google']) }}">
-                        <li class="content_auth_social-item go-item"></li>
-                    </a>
-                    <div class="clear"></div>
-                </ul>
-
-                <div class="content_auth_divider"></div>
-                <p>Еще не зарегистрированы?</p>
-                <a href="#">
-                    <button type="button" class="orange_button">Регистрация</button>
-                </a>
             </form>
+
+            <div class="content_auth_divider"></div>
+            <p>Войти, используя аккаунты в социальных сетях:</p>
+            <ul class="content_auth_social">
+                <a href="{{ route('socialize.request', ['provider' => 'facebook']) }}">
+                    <li class="content_auth_social-item fb-item"></li>
+                </a>
+                <a href="{{ route('socialize.request', ['provider' => 'vkontakte']) }}">
+                    <li class="content_auth_social-item vk-item"></li>
+                </a>
+                <a href="{{ route('socialize.request', ['provider' => 'twitter']) }}">
+                    <li class="content_auth_social-item tw-item"></li>
+                </a>
+                <a href="{{ route('socialize.request', ['provider' => 'google']) }}">
+                    <li class="content_auth_social-item go-item"></li>
+                </a>
+                <div class="clear"></div>
+            </ul>
+
+            <div class="content_auth_divider"></div>
+            <p>Еще не зарегистрированы?</p>
+            <a href="#">
+                <button type="button" class="orange_button">Регистрация</button>
+            </a>
         </div>
     </div>
 

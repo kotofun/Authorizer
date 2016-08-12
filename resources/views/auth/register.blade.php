3483705aff50ea6451374445246b1a05582dff2a
Дмитрий Басавин <basavind@gmail.com>
2016-08-12 11:38:49 +0300

    add errors displaying in register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,12 +5,18 @@
         <div class="content_auth">
             <h3>Регистрация</h3>
             <form method="POST" id="register-form" action="{{ route('auth.register.create') }}">
-                <input type="text" name="last_name" required="required" placeholder="Фамилия">
-                <input type="text" name="first_name" required="required" placeholder="Имя">
-                <input type="email" name="email" required="required" placeholder="Email"/>
+                <input type="text" name="last_name" required="required" placeholder="Фамилия" value="{{ $last_name or '' }}">
+                @include('auth.error', ['errors' => $errors->get('last_name')])
+                <input type="text" name="first_name" required="required" placeholder="Имя" value="{{ $first_name or '' }}">
+                @include('auth.error', ['errors' => $errors->get('first_name')])
+                <input type="email" name="email" required="required" placeholder="Email" value="{{ $email or '' }}"/>
+                @include('auth.error', ['errors' => $errors->get('email')])
                 <input type="email" name="email_confirmation" required="required" placeholder="Подтверждение Email"/>
+                @include('auth.error', ['errors' => $errors->get('email_confirmation')])
                 <input type="password" name="password" required="required" placeholder="Пароль"/>
+                @include('auth.error', ['errors' => $errors->get('password')])
                 <input type="password" name="password_confirmation" required="required" placeholder="Подтверждение пароля"/>
+                @include('auth.error', ['errors' => $errors->get('password_confirmation')])
                 <button type="submit" form="register-form" class="white_button">Зарегистрироваться</button>
             </form>
         </div>

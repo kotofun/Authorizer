bea3f278c952ee1e1b0c9e2d7ea6031aeafb70be
Дмитрий Басавин <basavind@gmail.com>
2016-10-11 23:18:07 +0300

    add redirect aliases

---
+++ config/redirects.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * @author Dmitry Basavin <basavind@gmail.com>
+ *         Date: 11.10.16
+ *         Time: 22:46
+ */
+
+return [
+    'help' => 'help',
+];

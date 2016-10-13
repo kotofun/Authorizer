6784a5ac250e4aae413cff9533736cc33f590e13
Дмитрий Басавин <basavind@gmail.com>
2016-10-13 09:25:53 +0300

    hide help info in help blade until user is authenticated

--- resources/views/help/show.blade.php
+++ resources/views/help/show.blade.php
@@ -5,89 +5,6 @@
 @section('content')
     <form method="post">
         <fieldset>
-            <legend>Хочу помочь: <span class="required">*</span></legend>
-            <ul class="help-list help-type">
-                <li class="help-list__item">
-                    <input type="radio" class="styled-box styled-box__radio with-sub-list" name="help_type"
-                           id="help_translate"
-                           value="help_translate">
-                    <label for="help_translate">Переводить</label>
-                    <ul class="help-list help-translate sub-list">
-                        @foreach([
-                            'Aeronautics/Astronautics',
-                            'Anthropology',
-                            'Architecture',
-                            'Athletics/Physical Education',
-                            'Biology/Biological Engineering',
-                            'Brain/Cognitive Sciences',
-                            'Chemistry/Chemical Engineering',
-                            'Civil & Environmental Engineering',
-                            'Comparative Media Studies',
-                            'Concourse',
-                            'Earth, Atmospheric & Planetary Sciences',
-                            'Economics',
-                            'Information Technology',
-                            'Circuits & Electronics',
-                            'Engineering Systems Division',
-                            'Experimental Study Group',
-                            'Global Studies & Languages',
-                            'Health Sciences & Technology',
-                            'History',
-                            'Linguistics & Philosophy',
-                            'Literature',
-                            'Materials Science & Engineering',
-                            'Mathematics',
-                            'Mechanical Engineering',
-                            'Media Arts & Sciences',
-                            'Music & Theater Arts',
-                            'Nanotechnology & Nanoscience',
-                            'Nuclear Science & Engineering',
-                            'Physics',
-                            'Political Science',
-                            'Science, Technology & Society',
-                            'Sloan School of Management',
-                            'Underactuated Robotics',
-                            'Urban Studies & Planning',
-                            'Writing & Humanistic Studies'
-                            ] as $section)
-                            <?php $snaked = snake_case(str_replace(['&', '/', ','], ' ', $section)); ?>
-                            <li class="help-list__item">
-                                <input type="checkbox" class="styled-box styled-box__check"
-                                       name="help_value[]" value="{{ $snaked }}"
-                                       id="{{ $snaked }}">
-                                <label for="{{ $snaked }}">{{ $section }}</label>
-                            </li>
-                        @endforeach
-                    </ul>
-                </li>
-                <li class="help-list__item">
-                    <input type="radio" class="styled-box styled-box__radio" name="help_type" id="help_edit"
-                           value="help_edit">
-                    <label for="help_edit">Редактировать</label>
-                </li>
-                <li class="help-list__item">
-                    <input type="radio" class="styled-box styled-box__radio" name="help_type" id="help_latex"
-                           value="help_latex">
-                    <label for="help_latex">Верстать в LaTeX</label>
-                </li>
-                <li class="help-list__item">
-                    <input type="radio" class="styled-box styled-box__radio" name="help_type" id="help_video"
-                           value="help_video">
-                    <label for="help_video">Заниматься видеомонтажом</label>
-                </li>
-                <li class="help-list__item">
-                    <input type="radio" class="styled-box styled-box__radio" name="help_type" id="help_develop"
-                           value="help_develop">
-                    <label for="help_develop">Заниматься разработкой</label>
-                </li>
-                <li class="help-list__item">
-                    <input type="radio" class="styled-box styled-box__radio" name="help_type" id="help_design"
-                           value="help_design">
-                    <label for="help_design">Заниматься дизайном</label>
-                </li>
-            </ul>
-        </fieldset>
-        <fieldset>
             <legend>Со мной можно связаться: <span class="required">*</span></legend>
             <ul class="social">
                 <li class="social__item{{ !empty($user) && in_array('facebook', $user['authored_providers']) ? " social__item_style_authored" : '' }}">
@@ -113,29 +30,116 @@
                 </li>
             </ul>
         </fieldset>
-        <fieldset>
-            <legend>Дополнительный контакт:</legend>
-            <div class="combobox">
-                <select name="additional_type" id="additional_type">
-                    <option value="email">Email</option>
-                    <option value="telegram">Telegram</option>
-                </select>
-                <div class="dropdown__container">
-                    <span class="dropdown__selected" data-value="email">Email</span>
-                    <div class="dropdown">
-                        <span class="dropdown__item dropdown__item_active" data-value="email">Email</span>
-                        <span class="dropdown__item" data-value="telegram">Telegram</span>
+        @if( !empty($user))
+            <fieldset>
+                <legend>Хочу помочь: <span class="required">*</span></legend>
+                <ul class="help-list help-type">
+                    <li class="help-list__item">
+                        <input type="radio" class="styled-box styled-box__radio with-sub-list" name="help_type"
+                               id="help_translate"
+                               value="help_translate">
+                        <label for="help_translate">Переводить</label>
+                        <ul class="help-list help-translate sub-list">
+                            @foreach([
+                                'Aeronautics/Astronautics',
+                                'Anthropology',
+                                'Architecture',
+                                'Athletics/Physical Education',
+                                'Biology/Biological Engineering',
+                                'Brain/Cognitive Sciences',
+                                'Chemistry/Chemical Engineering',
+                                'Civil & Environmental Engineering',
+                                'Comparative Media Studies',
+                                'Concourse',
+                                'Earth, Atmospheric & Planetary Sciences',
+                                'Economics',
+                                'Information Technology',
+                                'Circuits & Electronics',
+                                'Engineering Systems Division',
+                                'Experimental Study Group',
+                                'Global Studies & Languages',
+                                'Health Sciences & Technology',
+                                'History',
+                                'Linguistics & Philosophy',
+                                'Literature',
+                                'Materials Science & Engineering',
+                                'Mathematics',
+                                'Mechanical Engineering',
+                                'Media Arts & Sciences',
+                                'Music & Theater Arts',
+                                'Nanotechnology & Nanoscience',
+                                'Nuclear Science & Engineering',
+                                'Physics',
+                                'Political Science',
+                                'Science, Technology & Society',
+                                'Sloan School of Management',
+                                'Underactuated Robotics',
+                                'Urban Studies & Planning',
+                                'Writing & Humanistic Studies'
+                                ] as $section)
+                                <?php $snaked = snake_case(str_replace(['&', '/', ','], ' ', $section)); ?>
+                                <li class="help-list__item">
+                                    <input type="checkbox" class="styled-box styled-box__check"
+                                           name="help_value[]" value="{{ $snaked }}"
+                                           id="{{ $snaked }}">
+                                    <label for="{{ $snaked }}">{{ $section }}</label>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </li>
+                    <li class="help-list__item">
+                        <input type="radio" class="styled-box styled-box__radio" name="help_type" id="help_edit"
+                               value="help_edit">
+                        <label for="help_edit">Редактировать</label>
+                    </li>
+                    <li class="help-list__item">
+                        <input type="radio" class="styled-box styled-box__radio" name="help_type" id="help_latex"
+                               value="help_latex">
+                        <label for="help_latex">Верстать в LaTeX</label>
+                    </li>
+                    <li class="help-list__item">
+                        <input type="radio" class="styled-box styled-box__radio" name="help_type" id="help_video"
+                               value="help_video">
+                        <label for="help_video">Заниматься видеомонтажом</label>
+                    </li>
+                    <li class="help-list__item">
+                        <input type="radio" class="styled-box styled-box__radio" name="help_type" id="help_develop"
+                               value="help_develop">
+                        <label for="help_develop">Заниматься разработкой</label>
+                    </li>
+                    <li class="help-list__item">
+                        <input type="radio" class="styled-box styled-box__radio" name="help_type" id="help_design"
+                               value="help_design">
+                        <label for="help_design">Заниматься дизайном</label>
+                    </li>
+                </ul>
+            </fieldset>
+            <fieldset>
+                <legend>Дополнительный контакт:</legend>
+                <div class="combobox">
+                    <select name="additional_type" id="additional_type">
+                        <option value="email">Email</option>
+                        <option value="telegram">Telegram</option>
+                    </select>
+                    <div class="dropdown__container">
+                        <span class="dropdown__selected" data-value="email">Email</span>
+                        <div class="dropdown">
+                            <span class="dropdown__item dropdown__item_active" data-value="email">Email</span>
+                            <span class="dropdown__item" data-value="telegram">Telegram</span>
+                        </div>
                     </div>
+                    <input type="text" name="additional_value" class="combobox__input">
                 </div>
-                <input type="text" name="additional_value" class="combobox__input">
-            </div>
-        </fieldset>
-        <fieldset>
-            <legend>Комментарий:</legend>
-            <textarea name="comment" id="comment" rows="10"></textarea>
-        </fieldset>
-        <input type="submit" id="submit" value="Регистрация"{{ !empty($user)?:' disabled="disabled"' }}><br>
-        <span class="description"><span class="required">*</span> - Обязательное к заполнению поле</span>
+            </fieldset>
+            <fieldset>
+                <legend>Комментарий:</legend>
+                <textarea name="comment" id="comment" rows="10"></textarea>
+            </fieldset>
+            <input type="submit" id="submit" value="Помочь"{{ !empty($user)?:' disabled="disabled"' }}><br>
+            <span class="description"><span class="required">*</span> - Обязательное к заполнению поле</span>
+        @else
+            <p>Нам необходимо знать хотя бы один из ваших аккаунтов, чтобы у нас была возможность связаться с вами.</p>
+        @endif
     </form>
     @include('help.scripts')
 @endsection

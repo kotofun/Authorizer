7dbaee8af19e0f0059d817f3ae46ff68e4b75d32
Дмитрий Басавин <basavind@gmail.com>
2016-10-13 09:17:14 +0300

    refactor HelpController

--- app/Http/Controllers/HelpController.php
+++ app/Http/Controllers/HelpController.php
@@ -23,34 +23,53 @@ class HelpController extends Controller
         $this->tokenizer = $tokenizer;
     }
 
+    /**
+     * Index controller's action.
+     *
+     * It checks if user is authenticated and gets him if he is.
+     * If user is authenticated and exists it checks if user has
+     * already filled out help form and shows then thanks or help form.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\View\View
+     */
     public function index(Request $request)
     {
-        $token = Cookie::get('token');
-        $user = $this->tryGetUserFrom($token);
-        $helpAccepted = false;
+        $user = $this->getUserFromCookie();
 
-        if ( ! is_null($user)) {
-            if (count($user->info)) {
-
-                $helpAccepted = true;
-            } else if ($request->method() === 'POST') {
-                $helpAccepted = $this->acceptHelp($request, $user);
-            }
-        }
-
-        if ($helpAccepted) {
-            return view('help.thanks');
+        if ( ! is_null($user) && $this->isHelpAccepted($request, $user)) {
+            return $this->showThanks();
         }
 
         return $this->showHelpForm($user);
     }
 
     /**
-     * @param $token
+     * Try to get user from cookie.
      *
-     * @return mixed
+     * Since Lumen is stateless and sessionless framework it's not recommended
+     * to store user in sessions, that's why it uses cookie files to hold info
+     * about authenticated users.
+     *
+     * @return User|null
      */
-    private function tryGetUserFrom($token)
+    private function getUserFromCookie()
+    {
+        $token = Cookie::get('token');
+        $user = $this->tryExtractUserFrom($token);
+
+        return $user;
+    }
+
+    /**
+     * Try get user from jwt token.
+     *
+     * @param string|null $token
+     *
+     * @return \App\User|null
+     */
+    private function tryExtractUserFrom($token)
     {
         try {
             $user = $this->tokenizer->userFrom($token);
@@ -61,6 +80,40 @@ class HelpController extends Controller
         return $user;
     }
 
+    /**
+     * Check if users help is already accepted.
+     *
+     * We should not allow user to help twice right now,
+     * later it can be changed in personal page.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\User                $user
+     *
+     * @return bool
+     */
+    private function isHelpAccepted(Request $request, User $user)
+    {
+        $helpAccepted = false;
+
+        if ( ! is_null($user)) {
+            if (count($user->info)) {
+                $helpAccepted = true;
+            } elseif ($request->method() === 'POST') {
+                $helpAccepted = $this->acceptHelp($request, $user);
+            }
+        }
+
+        return $helpAccepted;
+    }
+
+    /**
+     * Create UserInfo record from request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\User                $user
+     *
+     * @return bool
+     */
     private function acceptHelp(Request $request, User $user)
     {
         $user->info()->create([
@@ -74,6 +127,21 @@ class HelpController extends Controller
     }
 
     /**
+     * Show thanks view.
+     *
+     * @return \Illuminate\View\View
+     */
+    private function showThanks()
+    {
+        return view('help.thanks');
+    }
+
+    /**
+     * Show help form for user.
+     *
+     * Some fields of help form should be visible only
+     * for existed and authored users.
+     *
      * @param \App\User|null $user
      *
      * @return \Illuminate\View\View
